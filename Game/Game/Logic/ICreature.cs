c5183a9039ae8dce6493bc984c211b046ee381be
Pavel <kamushekp@mail.ru>
2017-11-15 19:09:02 +0700

    creting interfaces

--- Game/Game/Logic/ICreature.cs
+++ Game/Game/Logic/ICreature.cs
@@ -6,5 +6,13 @@ namespace Game.Logic
 {
     interface ICreature
     {
+        Location Located { get; }
+        bool IsActive { get; }
+
+        string GetImageFileName();
+        int GetDrawingPriority();
+        List<IInnerComand> GetWhatCanDo();
+        List<UserComand> GetWhatReactingOn();
+
     }
 }
---
+++ Game/Game/Logic/IUserComand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Game.Logic
+{
+    class UserComand
+    {
+        public EventArgs Comand { get;}
+        public UserComand(EventArgs comand)
+        {
+            this.Comand = comand;
+        }
+    }
+}
